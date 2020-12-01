584f3e12e12d4a3112dd7e26c3479b80bb5678d9
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2020-12-01 15:43:31 +0100

    Reactivated logger

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 ﻿const convert = require('./lib/convert-document');
-//const logger = require('./lib/logger');
+const logger = require('./lib/logger');
 
 module.exports = (data, language = 'nb') => {
-    //logger('info', ['index', data._id, 'start']);
-    console.log('index', 'index', data._id, 'start');
+    logger('info', ['index', data._id, 'start']);
     try {
         const document = convert(data, language);
-        //logger('info', ['index', data._id, 'finished']);
-        console.log('index', data._id, 'finished');
+        logger('info', ['index', data._id, 'finished']);
         return document;
     }
     catch (error) {
-        //logger('error', ['index', 'error', JSON.stringify(error)]);
-        console.error('index', 'error', JSON.stringify(error));
+        logger('error', ['index', 'error', JSON.stringify(error)]);
         throw error;
     };
 }
