fba9d00da78962230c51dfaf6af650f9460a97ff
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-01-21 20:43:07 +0100

    Rebrand

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-﻿if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
 module.exports = {
-  PAPERTRAIL_HOSTNAME: process.env.PAPERTRAIL_HOSTNAME || 'minelev-robot',
+  PAPERTRAIL_HOSTNAME: process.env.PAPERTRAIL_HOSTNAME || 'minelev-document-converter',
   PAPERTRAIL_HOST: process.env.PAPERTRAIL_HOST || 'logs.papertrailapp.com',
   PAPERTRAIL_PORT: process.env.PAPERTRAIL_PORT || 12345
 }
