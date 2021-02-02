e12ec2a2e93b227057140207b23532057386409a
Mats Andreassen <matsand@live.no>
2021-02-02 09:07:36 +0100

    Skriv ut hva som er gæli når noe går på trynet, er du snill..

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = (data, language = 'nb') => {
     logger('info', ['index', data._id, 'finished'])
     return document
   } catch (error) {
-    logger('error', ['index', 'error', JSON.stringify(error)])
+    logger('error', ['index', 'error', JSON.stringify(error.message)])
     throw error
   };
 }
